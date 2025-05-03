d1b29d95d6a1ade51802f58d2c4ad3a3680186a4
Tanay Morakhia <tdmorakhia26@gmail.com>
2025-05-03 18:24:17 +0000

    stage 7

--- src/main/java/http/RequestHandler.java
+++ src/main/java/http/RequestHandler.java
@@ -1,10 +1,14 @@
 package http;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
 
 public class RequestHandler {
     
@@ -29,7 +33,7 @@ public class RequestHandler {
             System.out.println("Msg received from client " + requestString);
 
             if (requestString != null && requestString.split(" ")[1].equals("/")) {
-                responseString = "HTTP/1.1 200 OK\r\n\r\n";
+                responseString = ResponseText.STATUS_OK;
             }else if(requestString.split(" ")[1].split("/")[1].equals("echo")){
                 responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
                                     + requestString.split(" ")[1].split("/")[2].length() 
@@ -41,8 +45,28 @@ public class RequestHandler {
                 responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
                                     + userAgent.length()
                                     + "\r\n\r\n" + userAgent;
+            }else if(requestString.split(" ")[1].startsWith("/file")){
+                String fileName = requestString.split(" ")[1].split("/")[2];
+                File file = new File("/tmp/" + fileName);
+                if(file.exists() && file.isFile()){
+
+                    // try (FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+                    //     // get the size of the file
+                    //     long fileSize = channel.size();    
+                    // }
+                    Scanner sc = new Scanner(file);
+
+                    String content = "";
+                    while(sc.hasNextLine()){
+                        content += sc.nextLine();
+                    }
+                    responseString = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:" 
+                                        + file.length() + "\r\n\r\n" + content;
+                }else{
+                    responseString = ResponseText.STATUS_404;
+                }
             }else {
-                responseString = "HTTP/1.1 404 Not Found\r\n\r\n";
+                responseString = ResponseText.STATUS_404;
             }
 
             out.println(responseString);
