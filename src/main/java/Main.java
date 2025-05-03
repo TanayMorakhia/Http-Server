6903c69f60ed3d825229e4d684d332d25da60eba
Tanay Morakhia <tdmorakhia26@gmail.com>
2025-05-03 14:48:13 +0000

    refactored code and stage 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import http.HttpServer;
 
 public class Main {
@@ -12,6 +5,7 @@ public class Main {
 		// You can use print statements as follows for debugging, they'll be visible when running tests.
 		System.out.println("Logs from your program will appear here!");
 		System.out.println("Starting http server....");
-		HttpServer http = new HttpServer(4221);
+		HttpServer server = new HttpServer(4221);
+		server.start();
 	}
 }
