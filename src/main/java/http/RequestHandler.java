c5f45f203e5791f23e43c7ceb07296443b1d1bb6
Tanay Morakhia <tdmorakhia26@gmail.com>
2025-05-31 12:54:21 +0000

    level 12

--- src/main/java/http/RequestHandler.java
+++ src/main/java/http/RequestHandler.java
@@ -31,110 +31,113 @@ public class RequestHandler {
             
             OutputStream out = clientSocket.getOutputStream();
 
-            String requestString, responseString;
+            while(true){
 
-            requestString = in.readLine();
-            byte[] compressedData = null;
+                String requestString, responseString;
+
+                requestString = in.readLine();
+                byte[] compressedData = null;
 
 
-            System.out.println("Msg received from client " + requestString);
+                System.out.println("Msg received from client " + requestString);
 
-            HashMap<String, String> reqHeaders = new HashMap<>();
+                HashMap<String, String> reqHeaders = new HashMap<>();
 
-            String temp;
+                String temp;
 
-            temp = in.readLine();
-
-            while(!temp.equals("")){
-                reqHeaders.put(temp.split(" ")[0], temp.split(" ", 2)[1]);
                 temp = in.readLine();
-            }
 
-            if (requestString != null && requestString.split(" ")[1].equals("/")) {
-                responseString = ResponseText.STATUS_OK;
-            }else if(requestString.split(" ")[1].split("/")[1].equals("echo")){
-
-                if(reqHeaders.containsKey("Accept-Encoding:") && reqHeaders.get("Accept-Encoding:").contains("gzip")){
-                    GzipCompressor gzipCompressor = new GzipCompressor();
-                    String data = requestString.split(" ")[1].split("/")[2];
-
-                    byte[] uncompressedData = data.getBytes();
-
-                    compressedData = gzipCompressor.gzipCompression(uncompressedData);
-                    
-                    responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " +
-                                            compressedData.length + "\r\n\r\n";
-                }else{
-                    responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
-                                    + requestString.split(" ")[1].split("/")[2].length() 
-                                    + "\r\n\r\n" + requestString.split(" ")[1].split("/")[2];
+                while(!temp.equals("")){
+                    reqHeaders.put(temp.split(" ")[0], temp.split(" ", 2)[1]);
+                    temp = in.readLine();
                 }
-            }else if(requestString.split(" ")[1].equals("/user-agent")){
-                String userAgent = reqHeaders.get("User-Agent:");
-                responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
-                                    + userAgent.length()
-                                    + "\r\n\r\n" + userAgent;
-            }else if(requestString.split(" ")[1].startsWith("/file")){
-                if(requestString.split(" ")[0].equals("GET")){
-                    String fileName = requestString.split(" ")[1].split("/")[2];
-                    File file = new File(dir, fileName);
-                    if(file.exists() && file.isFile()){
-    
-                        // try (FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
-                        //     // get the size of the file
-                        //     long fileSize = channel.size();    
-                        // }
-                        Scanner sc = new Scanner(file);
-    
-                        String content = "";
-                        while(sc.hasNextLine()){
-                            content += sc.nextLine();
-                        }
-                        responseString = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:" 
-                                            + file.length() + "\r\n\r\n" + content;
-    
-                        sc.close();
+
+                if (requestString != null && requestString.split(" ")[1].equals("/")) {
+                    responseString = ResponseText.STATUS_OK;
+                }else if(requestString.split(" ")[1].split("/")[1].equals("echo")){
+
+                    if(reqHeaders.containsKey("Accept-Encoding:") && reqHeaders.get("Accept-Encoding:").contains("gzip")){
+                        GzipCompressor gzipCompressor = new GzipCompressor();
+                        String data = requestString.split(" ")[1].split("/")[2];
+
+                        byte[] uncompressedData = data.getBytes();
+
+                        compressedData = gzipCompressor.gzipCompression(uncompressedData);
+                        
+                        responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " +
+                                                compressedData.length + "\r\n\r\n";
                     }else{
-                        responseString = ResponseText.STATUS_404;
+                        responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+                                        + requestString.split(" ")[1].split("/")[2].length() 
+                                        + "\r\n\r\n" + requestString.split(" ")[1].split("/")[2];
                     }
-                }else{
+                }else if(requestString.split(" ")[1].equals("/user-agent")){
+                    String userAgent = reqHeaders.get("User-Agent:");
+                    responseString = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+                                        + userAgent.length()
+                                        + "\r\n\r\n" + userAgent;
+                }else if(requestString.split(" ")[1].startsWith("/file")){
+                    if(requestString.split(" ")[0].equals("GET")){
+                        String fileName = requestString.split(" ")[1].split("/")[2];
+                        File file = new File(dir, fileName);
+                        if(file.exists() && file.isFile()){
+        
+                            // try (FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+                            //     // get the size of the file
+                            //     long fileSize = channel.size();    
+                            // }
+                            Scanner sc = new Scanner(file);
+        
+                            String content = "";
+                            while(sc.hasNextLine()){
+                                content += sc.nextLine();
+                            }
+                            responseString = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:" 
+                                                + file.length() + "\r\n\r\n" + content;
+        
+                            sc.close();
+                        }else{
+                            responseString = ResponseText.STATUS_404;
+                        }
+                    }else{
 
-                    String fileName = requestString.split(" ")[1].split("/")[2];
+                        String fileName = requestString.split(" ")[1].split("/")[2];
 
-                    int length = Integer.parseInt(reqHeaders.get("Content-Length:"));
+                        int length = Integer.parseInt(reqHeaders.get("Content-Length:"));
 
-                    // needed for read function
-                    char[] tempBuff = new char[length];
-                
-                    in.read(tempBuff, 0, length);
-                    System.out.println(String.valueOf(temp));
+                        // needed for read function
+                        char[] tempBuff = new char[length];
+                    
+                        in.read(tempBuff, 0, length);
+                        System.out.println(String.valueOf(temp));
 
-                    File file = new File(dir + fileName);
+                        File file = new File(dir + fileName);
 
-                    if(!file.exists()){
-                        file.createNewFile();
+                        if(!file.exists()){
+                            file.createNewFile();
+                        }
+
+                        PrintWriter write = new PrintWriter(file);
+                        write.print(String.valueOf(String.valueOf(tempBuff)));
+                    
+                        responseString = ResponseText.STATUS_201;
+
+                        write.close();
                     }
-
-                    PrintWriter write = new PrintWriter(file);
-                    write.print(String.valueOf(String.valueOf(tempBuff)));
-                
-                    responseString = ResponseText.STATUS_201;
-
-                    write.close();
+                }else {
+                    responseString = ResponseText.STATUS_404;
                 }
-            }else {
-                responseString = ResponseText.STATUS_404;
-            }
 
-            out.write(responseString.getBytes(StandardCharsets.UTF_8));
-            try{
-                if(compressedData.length != 0){
-                    out.write(compressedData);
+                out.write(responseString.getBytes(StandardCharsets.UTF_8));
+                try{
+                    if(compressedData.length != 0){
+                        out.write(compressedData);
+                    }
+                }catch(NullPointerException e){
+                    
                 }
-            }catch(NullPointerException e){
-                
+                System.out.println("Response message sent to client");
             }
-            System.out.println("Response message sent to client");
 
         }catch(IOException e){
             System.out.println(e.getMessage());
